fcf25e2c12c8f2b1090da3a7de54e9aeed8d41fa
Rafael.V <rafaelvpaz@gmail.com>
2024-11-06 12:07:20 -0300

    Adicionando metodos de identi para outliers usando zscore e IQR

--- scripts/calculo.py
+++ scripts/calculo.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 import os
+from scipy.stats import zscore
 
 # Carregar os dados
 try:
@@ -11,7 +14,12 @@ try:
     # Limpar dados, se necessário
     data.dropna(axis=1, how='all', inplace=True)  # Remove colunas vazias
     print("Colunas após limpeza:", data.columns)
-
+    
+    data.duplicated(['Name', 'Type1', 'Totalstats'])
+    data.duplicated().sum()
+    data.drop_duplicates()
+    print("Valores duplicados de colunas excluido com sucesso")
+    
     # Verifique se as colunas 'Totalstats' e 'Type1' existem
     if 'Totalstats' not in data.columns:
         print("Erro: A coluna 'Totalstats' não existe no arquivo.")
@@ -45,6 +53,56 @@ try:
         plt.show()  # Exibe o gráfico para verificar se está correto
         plt.savefig('outputs/grafico_dispercao.png')  # Salva o gráfico de dispersão
         print("Gráfico de dispersão salvo em 'outputs/grafico_dispercao.png'")
+        
+        # Criando um Boxplot para anlise de outliers 
+        
+        plt.figure(figsize=(8, 5))
+        sns.boxplot(data=data, x='Totalstats')
+        plt.title("Boxplot para identificação de outliers")
+        plt.show()
+        
+
+        # Calculando o Z-score
+        data['Z-Score'] = zscore(data['Totalstats'])
+
+        # Filtrando os dados com base no Z-score 
+        data_no_outliers_z = data[data['Z-Score'].abs() <= 3]
+
+        # Exibindo os dados sem outliers
+        print("Dados sem valores atípicos (Z-score):\n", data_no_outliers_z[['Totalstats']])
+
+        # Plotando o boxplot após a remoção dos outliers
+        plt.figure(figsize=(8, 5))
+        sns.boxplot(data=data_no_outliers_z, x='Totalstats')
+        plt.title("Boxplot sem Outliers (Z-score)")
+        plt.show()
+ 
+        print("Quantidade total de dados:", len(data))
+        print("Quantidade de dados após remoção de outliers pelo Z-score:", len(data_no_outliers_z))
+
+        # # Calculando o IQR para remover os outliers que são os valores fora do padrão
+        
+        # Q1 = data['Totalstats'].quantile(0.25)
+        # Q3 = data['Totalstats'].quantile(0.75)
+        # IQR = Q3 - Q1
+        
+        # # Definindo os limites para identificar os outliers (valores abaixo do lower ou em cima do upper são outliers)
+        
+        # lower_bound = Q1 - 1.5 * IQR
+        # upper_bound = Q3 + 1.5 * IQR
+        
+        # # filtrando para apenas pegar os valores que não são outliers
+        
+        # naosao_outliers = data[(data['Totalstats'] >= lower_bound) & (data['Totalstats'] <= upper_bound)]
+        
+        # print("Dados sem valores atípicos:\n", naosao_outliers)
+        
+        # # Plotando o boxplot após a remoção dos outliers
+        # plt.figure(figsize=(8, 5))
+        # sns.boxplot(data=naosao_outliers, x='Totalstats')
+        # plt.title("Boxplot sem Outliers")
+        # plt.show()
+
 
 except Exception as e:
     print("Erro ao carregar ou processar o arquivo:", e)
